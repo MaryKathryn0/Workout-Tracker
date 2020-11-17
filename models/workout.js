608e86bde7fdca1646a3c5600d3efe45e47b0020
Mary Kathryn <msmarykathrynholt@gmail.com>
2020-11-17 08:33:22 -0600

    worked on schema and attributes

---
+++ models/workout.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+    day: {
+        type: Date,
+        default: () => new Date()
+    },
+    exercises: [
+        {
+            name: {
+                type: String,
+                required: "Enter exercise name:"
+            },
+            type: {
+                type: String,
+                required: "Enter exercise type:"
+            },
+            weight: {
+                type: Number,
+            },
+            sets: {
+                type: Number,
+            },
+            reps: {
+                type: Number,
+            },
+            duration: {
+                type: Number,
+                required: "Enter how many minutes you exercised:"
+            },
+            distance: {
+                type: Number,
+            }
+        }
+    ]
+
+
+
+});
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
